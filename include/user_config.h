2dd663d2593e89c094aff2b4dcaa7c787e077c89
Tomasz Głuch <poczta@tomaszgluch.pl>
2015-03-17 19:48:43 +0100

    Updated configuration, WIFI credentials in separate file.

--- include/user_config.h
+++ include/user_config.h
@@ -1,23 +1,22 @@
 #ifndef _USER_CONFIG_H_
 #define _USER_CONFIG_H_
 
-#define CFG_HOLDER	0x00FF55A4	/* Change this value to load default configurations */
+#define CFG_HOLDER	0x00FF55AA	/* Change this value to load default configurations */
 #define CFG_LOCATION	0x3C	/* Please don't change or if you know what you doing */
 #define CLIENT_SSL_ENABLE
 
 /*DEFAULT CONFIGURATIONS*/
 
-#define MQTT_HOST			"192.168.11.122" //or "mqtt.yourdomain.com"
-#define MQTT_PORT			1880
+#define MQTT_HOST			"mqtt.tomaszgluch.pl" //or "mqtt.yourdomain.com"
+#define MQTT_PORT			1883
 #define MQTT_BUF_SIZE		1024
 #define MQTT_KEEPALIVE		120	 /*second*/
 
 #define MQTT_CLIENT_ID		"DVES_%08X"
-#define MQTT_USER			"DVES_USER"
-#define MQTT_PASS			"DVES_PASS"
+#define MQTT_USER			"esp1"
+#define MQTT_PASS			"qw12QW12"
 
-#define STA_SSID "DVES_HOME"
-#define STA_PASS "yourpassword"
+#include "user_config_wifi.h"
 #define STA_TYPE AUTH_WPA2_PSK
 
 #define MQTT_RECONNECT_TIMEOUT 	5	/*second*/
